50197a8e80e12ee58706824415695a36a6dce58a
rafi <rafie6195@gmail.com>
2023-11-10 16:10:34 +0700

    Pembuatan tabel Alamat Dan Tabel Negara

--- buat database retail_online dan pembuatan tabel customer.sql
+++ buat database retail_online dan pembuatan tabel customer.sql
@@ -8,3 +8,29 @@ create table retail_online.customer (
 
 );
 
+create table retail_online.country (
+    country_id int primary key  auto_increment,
+    country_name varchar(250)
+);
+
+create table  retail_online.address(
+    address_id int primary key auto_INCrement,
+    unit_number int ,
+    street_number int,
+    address_line1 varchar(250),
+    address_line2 varchar(250),
+    city varchar(50),
+    region varchar(50),
+    postal_code varchar(50),
+    country_id int ,
+    foreign key (country_id)  REFERENCES  country(country_id)
+)
+
+
+create table retail_online.customer_address(
+    customer_id int,
+    foreign key (customer_id) references  customer(customer_id),
+    address_id int,
+    foreign key (address_id) references  address(address_id),
+    is_default boolean
+);
